372de92acae4afb9318b95293877f4ae2868774c
Mike Huniewicz <michal.huniewicz@ft.com>
2014-12-08 13:48:08 +0000

    Reading the gallery year from user input.

--- generate_library.rb
+++ generate_library.rb
@@ -9,5 +9,20 @@ if gallery_title.empty?
   abort "Gallery title cannot be empty."
 end
 
-puts "Gallery title is [#{gallery_title}]"
+class String
+  def is_i?
+    Integer(self) != nil rescue false
+  end
+end
+
+puts "Gallery title is [#{gallery_title}]."
+puts
+puts "What is the gallery year?"
+gallery_year_s = gets.chomp.strip
+unless gallery_year_s.is_i?
+  abort "Gallery year needs to be a number."
+end
+gallery_year = Integer(gallery_year_s)
+
+puts "Gallery year is [#{gallery_year}]."
 
