91c0cdcfa71de1608d5248df832adbc7fab2d1a3
Mike Huniewicz <michal.huniewicz@ft.com>
2014-12-08 16:16:20 +0000

    Set default gallery year to current year.

--- generate_library.rb
+++ generate_library.rb
@@ -1,9 +1,11 @@
+require 'date'
+
 puts "Gallery Generator"
 puts "www.m1key.me"
 puts "This generates a m1key.me style gallery HTML code."
 puts
-puts "What is the gallery title?"
 
+puts "What is the gallery title?"
 gallery_title = gets.chomp.strip
 if gallery_title.empty? 
   abort "Gallery title cannot be empty."
@@ -14,19 +16,23 @@ class String
     Integer(self) != nil rescue false
   end
 end
-
 puts "Gallery title is [#{gallery_title}]."
 puts
-puts "What is the gallery year?"
+
+current_year = Date.today.strftime("%Y")
+
+puts "What is the gallery year? [#{current_year}]"
 gallery_year_s = gets.chomp.strip
-unless gallery_year_s.is_i?
-  abort "Gallery year needs to be a number."
+if gallery_year_s.empty?
+  gallery_year = Integer(current_year)
+else
+  unless gallery_year_s.is_i?
+    abort "Gallery year needs to be a number."
+  end
+  gallery_year = Integer(gallery_year_s)
+  unless gallery_year > 999 and gallery_year < 10000
+    abort "Gallery year should be exactly four digits long."
+  end
 end
-gallery_year = Integer(gallery_year_s)
-
-unless gallery_year > 999 and gallery_year < 10000
-  abort "Gallery year should be four digits."
-end
-
 puts "Gallery year is [#{gallery_year}]."
 
