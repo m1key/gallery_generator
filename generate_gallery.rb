e3feaa902e9a8991b5e57c0282e8fd0cee3a100e
Mike Huniewicz <michal.huniewicz@ft.com>
2014-12-11 17:37:22 +0000

    Reading exposure time from image EXIF metadata.

--- generate_gallery.rb
+++ generate_gallery.rb
@@ -90,7 +90,8 @@ def get_metadata_for_image_with_file_name_containing(photo_file_name_contains)
   photo_iso = EXIFR::JPEG.new(selected_file_name).iso_speed_ratings
   photo_focal_length = EXIFR::JPEG.new(selected_file_name).focal_length.to_f.round.to_s
   photo_f_number = EXIFR::JPEG.new(selected_file_name).f_number.to_f
-  return Metadata.new(photo_height, photo_iso, photo_focal_length, photo_f_number)
+  photo_exposure_time = EXIFR::JPEG.new(selected_file_name).exposure_time.to_s
+  return Metadata.new(photo_height, photo_iso, photo_focal_length, photo_f_number, photo_exposure_time)
 end
 
 photos = []
--- metadata.rb
+++ metadata.rb
@@ -1,10 +1,11 @@
 class Metadata
-  attr_reader :height, :iso, :focal_length, :f_number
-  def initialize(height, iso, focal_length, f_number)
+  attr_reader :height, :iso, :focal_length, :f_number, :exposure_time
+  def initialize(height, iso, focal_length, f_number, exposure_time)
     @height = height
     @iso = iso
     @focal_length = focal_length
     @f_number = f_number
+    @exposure_time = exposure_time
   end
 end
 
