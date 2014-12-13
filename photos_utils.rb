b668c969405eeee0343ed9bf4c4de204c114ba8c
Michal Huniewicz <michal.huniewicz.registered@gmail.com>
2014-12-13 16:19:03 +0000

    Added photos to ViewableGallery.

---
+++ photos_utils.rb
@@ -0,0 +1,67 @@
+def load_photos_info(gallery_config)
+  photos = []
+  current_photo_number = 0
+  total_photos_number = gallery_config.total_photos_number
+  photo_id_digits = gallery_config.photo_id_digits
+  puts "There are [#{total_photos_number}] photos total."
+  gallery_config.photos.each do |photo|
+    current_photo_number += 1
+    photo_id = to_photo_id(current_photo_number, photo_id_digits)
+    photo_title = photo["title"]
+    photo_description = add_links_to_sources(remove_final_empty_line(add_tabs_before_every_line(photo["description"], 3)))
+    photo_file_name_contains = photo["fileNameContains"]
+    photo_metadata = get_metadata_for_image_with_file_name_containing(photo_file_name_contains)
+
+    puts "Adding photo with ID [#{photo_id}], title [#{photo_title}], height [#{photo_metadata.height}], description [#{compact(photo["description"])}]..."
+    photos.push Photo.new(photo_id, photo_title, photo_description, photo_metadata)
+
+    create_gallery_image(photo_metadata.original_file_name, gallery_config.slug, photo_id)
+  end
+  return photos
+end
+
+def to_photo_id(current_photo_number, photo_id_digits)
+  photo_id = "#{current_photo_number}"
+  while photo_id.size < photo_id_digits
+    photo_id = "0" + photo_id
+  end
+  return photo_id
+end
+
+def get_metadata_for_image_with_file_name_containing(photo_file_name_contains)
+  selected_file_name = ""
+  Dir.entries(".").each do |file_name|
+    if file_name.include? photo_file_name_contains.to_s
+      unless selected_file_name == ""
+        puts "WARN  More than one file name matches [#{photo_file_name_contains}]. Will use the last one that matches."
+      end
+      selected_file_name = file_name
+    end
+  end
+  if selected_file_name == ""
+    puts "WARN  No matching photo found for #{photo_file_name_contains}."
+    return
+  end
+ 
+  exif = EXIFR::JPEG.new(selected_file_name)
+  photo_height = exif.height
+  photo_iso = exif.iso_speed_ratings
+  photo_focal_length = exif.focal_length.to_f.round.to_s
+  photo_f_number = exif.f_number.to_f
+  photo_exposure_time = exif.exposure_time.to_s
+  return Metadata.new(selected_file_name, photo_height, photo_iso, photo_focal_length, photo_f_number, photo_exposure_time)
+end
+
+def create_gallery_image(original_file_name, gallery_slug, photo_id)
+  FileUtils.cp(original_file_name, gallery_slug + "_" + photo_id + File.extname(original_file_name))
+end
+
+def add_links_to_sources(multi_line_string)
+  result = ""
+  multi_line_string.each_line do |line|
+    line_with_empty_line_removed =  line.gsub(/\[(\d)\]/, '[<a href="#sources">\1</a>]')
+    result += line_with_empty_line_removed
+  end
+  return result
+end
+
--- viewable_gallery.rb
+++ viewable_gallery.rb
@@ -1,8 +1,8 @@
 require_relative 'string_utils'
 
 class ViewableGallery
-  attr_reader :title, :description, :slug, :sources, :upload_date, :map_url, :map_title, :year
-  def initialize(gallery_config)
+  attr_reader :title, :description, :slug, :sources, :upload_date, :map_url, :map_title, :year, :photos
+  def initialize(gallery_config, photos)
     @title = gallery_config.title
     @description = add_tabs_before_every_line(gallery_config.description, 2)
     @slug = gallery_config.slug
@@ -11,5 +11,6 @@ class ViewableGallery
     @map_url = gallery_config.map_url
     @map_title = gallery_config.map_title
     @year = gallery_config.year
+    @photos = photos
   end
 end
